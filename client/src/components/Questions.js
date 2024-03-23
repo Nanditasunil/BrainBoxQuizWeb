63f8872cb827e9192ffb19083e69dcdfb14a111a
Nanditasunil <109030620+Nanditasunil@users.noreply.github.com>
2024-03-23 21:24:19 +0530

    done work on prev and next button

--- client/src/components/Questions.js
+++ client/src/components/Questions.js
@@ -1,22 +1,46 @@
 import React, { useEffect, useState } from "react";
-import data from "../database/data";
+// import data from "../database/data";
+
+import { useSelector } from "react-redux";
+
+// custom hooks
+import { useFetchQuestion } from "../hooks/FetchQuestion";
+
 export default function Questions() {
   const [checked, setChecked] = useState(undefined);
+  const [{ isLoading, apiData, serverError }] = useFetchQuestion();
+  // const question = data[0];
 
-  const question = data[0];
+  const questions = useSelector(
+    (state) => state.questions.queue[state.questions.trace]
+  );
+  const trace = useSelector((state) => state.questions.trace);
+
   useEffect(() => {
-    // console.log(question);
+    // console.log(questions);
   });
+
+  // useEffect(() => {
+  //   // console.log(question);
+  //   // console.log(isLoading);
+  //   // console.log(apiData);
+  //   // console.log(serverError);
+  // });
 
   function onSelect() {
     // setChecked(true);
     // console.log("radio button changed ");
   }
+
+  if (isLoading) return <h3 className="text-light">isLoading</h3>;
+  if (serverError)
+    return <h3 className="text-light">{serverError || "Unknown Error"}</h3>;
+
   return (
     <div className="questions">
-      <h2 className="text-light">{question.question}</h2>
-      <ul key={question.id}>
-        {question.options.map((q, i) => (
+      <h2 className="text-light">{questions?.question}</h2>
+      <ul key={questions?.id}>
+        {questions?.options.map((q, i) => (
           <li key={i}>
             <input
               type="radio"
--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -2,19 +2,31 @@ import React, { useEffect } from "react";
 import Questions from "./Questions";
 
 // redux store import
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { moveNextQuestion, movePrevQuestion } from "../hooks/FetchQuestion";
 export default function Quiz() {
-  const state = useSelector((state) => state);
+  // const trace = useSelector((state) => state.questions.trace);
+
+  const { queue, trace } = useSelector((state) => state.questions);
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    console.log(state);
+    console.log(trace);
   });
 
   function onNext() {
     console.log("On Next Click");
+    // update trace value by 1
+    if (trace < queue.length) {
+      dispatch(moveNextQuestion());
+    }
   }
   function onPrev() {
     console.log("On Prev Click");
+    if (trace > 0) {
+      dispatch(movePrevQuestion());
+    }
   }
   return (
     <div className="container">
